b020aa8113fb00b3bf23d201fd868a8fc5f7e43c
Magdalena Tarka <mxtarka@gmail.com>
2021-05-15 09:53:08 +0200

    Display oreder option of trip in Trip view, add components: OrederOptionCheckboxes, OrderOptionDropdown, OrderOptionIcons, OrderOptionNumber

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types';
 //import styles from './OrderForm.scss';
 import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
-const OrderForm = props => (
+const OrderForm = (props) => (
   <Row>
+    {pricing.map(option => (
+      <Col md={4} key={option.id} {...option}>
+        <OrderOption {...option} />
+      </Col>
+    ))}
     <Col xs={12}>
       <OrderSummary tripCost={props.tripCost} options={props.options} />
     </Col>
